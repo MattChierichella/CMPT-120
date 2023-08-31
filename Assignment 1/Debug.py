e66848faee87ca02be7a56c2a5e144f12bdf6a28
MattChierichella <143512505+MattChierichella@users.noreply.github.com>
2023-08-31 08:44:33 -0400

    Pt2

--- Assignment 1/Debug.py
+++ Assignment 1/Debug.py
@@ -16,7 +16,6 @@ def main():
 
     #insert your name into this variable
     name = ""
-    print("Hello, my name is ", name)
     print("Hello, my ame is ", name)
 
     #insert your best friend's name here and have it print out
