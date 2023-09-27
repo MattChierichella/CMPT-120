10a9aa024c404e3cc2547e9ba00e56b17098a6e9
MattChierichella <143512505+MattChierichella@users.noreply.github.com>
2023-09-27 13:28:24 -0400

    Doing some hw
    hsodvhjs

--- Assignment 2.5/if_else_sign_practice.py
+++ Assignment 2.5/if_else_sign_practice.py
@@ -5,10 +5,13 @@ def main():
         If they're 18 and older but younger than 21, tell them they can only buy nicotine products in some states
         If they're less than 18, they can only purchase candy cigarettes and sody pops
     '''
-    
-  
-
-  
-  
-  
+    age = input("What is your age?")
+    if int(age) >= 25:
+        print("You can buy alcohol, nicotine products, and you can rent a car!")
+    elif 25 > int(age) >= 21:
+        print("You can buy alcohol and nicotine products, but cannot rent a car")
+    elif 21 > int(age) >= 18:
+        print("You can only buy nicotine products in some states")
+    elif int(age) < 18:
+        print("You can only purchase candy cigarettes and sody pops")
 main()
