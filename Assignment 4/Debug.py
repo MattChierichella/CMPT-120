10bcdd61cf68334f4bcfc79ba4008de4cd32ee03
MattChierichella <143512505+MattChierichella@users.noreply.github.com>
2023-11-05 16:31:13 -0500

    Homework Completed

--- Assignment 4/Debug.py
+++ Assignment 4/Debug.py
@@ -1,38 +1,35 @@
 
-def printHello():
+def printHello(y):
     print("Hello")
-    
+    return
 def printName(x):
     print(x)
-    
+
 def addition(x, y):
     #add x and y together and return them
+    print(y, x)
     return
-
 def smaller(i, j):
     #if i is smaller than j, return i
+    if i < j:
+        return i
     #if j is smaller than i, return j
+    elif j < i:
+        return j
     #if they're even, return 0
-
+    elif j == i:
+        return 0
 def main():
-    
     #call the printHello function here
-    
+    y = printHello("Hello")
     #call printName and give it the parameter of your name
-    
-    var1= 10
-    var2= 20
+    x = printName("Matthew")
+    var1 = 10
+    var2 = 20
     #What do we put in here to make it work?
-    print(addition())
-    
-    num1 = int(input("Enter number 1"))
-    num2 = int(input("Enter number 2"))
+    print(addition(var1, var2))
+    i = int(input("Enter number 1"))
+    j = int(input("Enter number 2"))
     #what go here?
-    print(smaller())
-
-
-
-
-
-
+    print(smaller(i, j))
 main()
--- Assignment 4/blackjack.py
+++ Assignment 4/blackjack.py
@@ -1,12 +1,25 @@
 #if you don't know how to play blackjack, tbh not super important but look it up anyway LOL
 #this script is going to require some googling: I want you to practice using your resources with this one. But of course if you get stuck, reach out :)
 '''instructions: randomly generate three values between 1 and 11. in the function bust: add these three numbers together. if they add up to or less than 21, return the sum. If it's over 21, return 0. If it's over 21 BUT there's an 11 as one of the values, return the sum - 10. '''
+import random
+
 
 def bust():
-    
-    
-    
-def main():
-    
+    mycard = []
+    for y in range(3):
+        num = random.randint(1, 11)
+        mycard.append(num)
+    z = sum(mycard)
+    if int(z) <= 21:
+        print(z)
+    elif 11 in mycard and z > 21:
+        print(int(z) - 10)
+    elif int(z) >= 21:
+        print(0)
 
-main()
+bust()
+
+def main():
+
+
+    main()
